5215d39f981ff64841c4627652528d072d8f5c40
Oskar Codes <oskarbertrand@icloud.com>
2025-01-08 20:16:16 +0100

    Rework project structure

---
+++ src/assets/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3oZkLGgL1zJCd1emC6AL6Rn4hf_t47FI",
+  authDomain: "card-games-b8a4e.firebaseapp.com",
+  projectId: "card-games-b8a4e",
+  storageBucket: "card-games-b8a4e.firebasestorage.app",
+  messagingSenderId: "905688075182",
+  appId: "1:905688075182:web:7ec879f764a6bb1c5ed63f",
+  measurementId: "G-YDW4CW73VT"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
---
+++ src/stores/store.js
@@ -0,0 +1,5 @@
+import { db } from '@/assets/js/firebase'
+
+export const useStore = defineStore('store', () => {
+  
+})
