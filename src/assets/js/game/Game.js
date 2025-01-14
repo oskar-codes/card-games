ac0be625602d490b09ca5f134c7b31484d228d4e
Oskar Codes <oskarbertrand@icloud.com>
2025-01-14 17:30:01 +0100

    Add update listeners

--- src/assets/js/game/Game.js
+++ src/assets/js/game/Game.js
@@ -1,5 +1,5 @@
 import { db } from '../firebase';
-import { collection, addDoc, doc, setDoc, getDoc } from 'firebase/firestore';
+import { onSnapshot, doc, setDoc, getDoc } from 'firebase/firestore';
 import { v4 as uuidv4 } from 'uuid';
 import { Player } from './Player';
 
@@ -22,6 +22,8 @@ export class Game {
     this.players = players;
     this.currentPlayerIndex = 0;
     this.currentCard = null;
+
+    this.unsubscribeListener = null;
   }
 
   serialized() {
@@ -47,24 +49,56 @@ export class Game {
 
 
   static async createGame(host) {
-    console.log(host);
     const id = uuidv4();
     const game = new Game(id, host ? [host] : []);
     await setDoc(doc(db, 'games', id), game.serialized());
 
+    game.addUpdateListener();
+
     return game;
   }
 
-  static async loadGame(id) {
+  static async joinGame(id, player) {
     const docRef = doc(db, 'games', id);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
       const data = docSnap.data();
-      return Game.hydrate(data);
+      const game = Game.hydrate(data);
+
+      // Add player to game if not already in game
+      if (!game.players.find(p => p.id === player.id)) {
+        game.players.push(player);
+        await game.updateGameOnFirestore();
+      }
+
+      game.addUpdateListener();
+
+      return game;
     }
 
     return null;
+  }
+
+  addUpdateListener() {
+    if (this.unsubscribeListener) {
+      return;
+    }
+
+    const docRef = doc(db, 'games', this.id);
+    this.unsubscribeListener = onSnapshot(docRef, (doc) => {
+      const data = doc.data();
+      const hydrated = Game.hydrate(data);
+
+      // Assign each players individually
+      this.players = [];
+      hydrated.players.forEach(player => {
+        this.players.push(player);
+      });
+      
+      this.currentPlayerIndex = hydrated.currentPlayerIndex;
+      this.currentCard = hydrated.currentCard;
+    });
   }
 
 }
