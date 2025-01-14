b43fb1c656f553b07128655843f45c0356203443
Oskar Codes <oskarbertrand@icloud.com>
2025-01-14 16:47:08 +0100

    locally store player id, and enable game loading

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,16 +1,33 @@
 import { Game } from '@/assets/js/game/Game'
+import { Player } from '@/assets/js/game/Player'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
 
 export const useStore = defineStore('store', () => {
-  const game = ref(null)
+  const game = ref(null);
+  const player = ref(null);
+
+  const localPlayer = localStorage.getItem('local-player');
+  if (localPlayer) {
+    const data = JSON.parse(localPlayer);
+    player.value = new Player(data.id ?? uuidv4(), data.name ?? 'Guest');
+  } else {
+    player.value = new Player(uuidv4(), 'Guest');
+    localStorage.setItem('local-player', JSON.stringify(player.value.serialized()));
+  }
 
   const createGame = async () => {
-    game.value = await Game.createGame();
+    game.value = await Game.createGame(player.value);
+  }
+
+  const loadGame = async (id) => {
+    game.value = await Game.loadGame(id);
   }
 
   return {
     game,
-    createGame
+    createGame,
+    loadGame
   }
 })
