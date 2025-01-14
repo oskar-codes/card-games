a2b8f97656f20856cf68d1bf447764c3107d18e6
Oskar Codes <oskarbertrand@icloud.com>
2025-01-14 16:45:58 +0100

    Add IDs to players

--- src/assets/js/game/Player.js
+++ src/assets/js/game/Player.js
@@ -1,22 +1,28 @@
+import { Card } from './Card';
+
 /**
  * @class Player
  *
+ * @property {String} id
  * @property {String} name
  * @property {Card[]} hand
  */
 export class Player {
 
   /**
+   * @param {String} id
    * @param {String} name
    * @param {Card[]} hand
    */
-  constructor(name, hand = []) {
+  constructor(id = uuidv4(), name, hand = []) {
+    this.id = id;
     this.name = name;
     this.hand = hand;
   }
 
   serialized() {
     return {
+      id: this.id,
       name: this.name,
       hand: this.hand.map(card => card.serialized())
     }
