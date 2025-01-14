614b0714ee85ca5aa96be28d14b0a2ae7e5339ea
Oskar Codes <oskarbertrand@icloud.com>
2025-01-14 16:47:29 +0100

    Create raw lobby view

--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,11 +1,34 @@
 <template>
   <h1>Lobby</h1>
+
+  <p>Players:</p>
+  <ul>
+    <li v-for="player in game.players" :key="player.id">
+      {{ player.name }}
+    </li>
+  </ul>
 </template>
 
 <script>
-import { useGameStore } from '@/stores/store';
+import { useStore } from '@/stores/store';
+import { useRoute } from 'vue-router';
 
 export default {
-  
+  async setup() {
+    const store = useStore();
+    
+    const route = useRoute();
+    const gameId = route.params.id;
+
+    await store.loadGame(gameId);
+
+    if (!store.game) {
+      this.$router.push('/');
+    }
+
+    return {
+      game: store.game,
+    };
+  },
 }
 </script>
